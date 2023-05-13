28d3eb45bfe1806978899f6cd475f4049109dbc7
Vanshita-31 <vanshita.dhanawade31@gmail.com>
2023-05-13 12:25:03 +0530

    Java Variables: Widening

---
+++ widening.java
@@ -0,0 +1,8 @@
+public class widening {
+    public static void main(String[] args) {
+        int a = 30;
+        float b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
