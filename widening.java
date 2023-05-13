e190d2c80a1459edcfb29f1ec754ddfb18fa7ce8
Vanshita-31 <vanshita.dhanawade31@gmail.com>
2023-05-13 12:32:03 +0530

    Java variables: Widening , Type Casting

--- widening.java
+++ widening.java
@@ -1,8 +1,17 @@
 public class widening {
     public static void main(String[] args) {
         int a = 30;
-        float b = a;
+        float b = a;// widening
         System.out.println(a);
         System.out.println(b);
+
+        // Type casting
+
+        float f = 10.5f;
+        // int a = f;//it will give an error coz it cant convert float into int so we
+        // have to type cast it..
+        int c = (int) f;
+        System.out.println(c);
+
     }
 }
